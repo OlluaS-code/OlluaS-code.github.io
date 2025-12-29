a2519ad85d5ebf72cbdffae351f6e7f9b42cdd17
OlluaS-code <s.moura.tav@gmail.com>
2025-12-29 14:26:32 -0300

    fix: Add Css Error in Input form

--- Client/src/App.ts
+++ Client/src/App.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const form = document.getElementById("meuFormulario") as HTMLFormElement;
 const btnEnviar = document.getElementById("btnEnviar") as HTMLButtonElement;
 const statusDiv = document.getElementById("statusFeedback") as HTMLDivElement;
+const fields = document.querySelectorAll(".mb-4");
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -12,14 +13,18 @@ form.addEventListener("submit", async (event: Event) => {
   btnEnviar.disabled = true;
   btnEnviar.innerHTML = 'Enviando... <i class="ri-loader-4-line"></i>';
   statusDiv.textContent = "";
+  fields.forEach((field) => field.classList.remove("field-error"));
 
   const formData = new FormData(form);
+  const emailInput = document.getElementById("email") as HTMLInputElement;
+  const nameInput = document.getElementById("nome") as HTMLInputElement;
+  const messageInput = document.getElementById(
+    "mensagem"
+  ) as HTMLTextAreaElement;
 
   try {
     const response = await axios.post(`${API_URL}/contact-us`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
+      headers: { "Content-Type": "multipart/form-data" },
     });
 
     if (response.data.success) {
@@ -29,6 +34,22 @@ form.addEventListener("submit", async (event: Event) => {
     const msgErro =
       error.response?.data?.message || "Erro ao conectar com o servidor.";
     statusDiv.innerHTML = `<span style="color: #dc3545;">${msgErro}</span>`;
+
+    if (
+      msgErro.toLowerCase().includes("e-mail") ||
+      msgErro.toLowerCase().includes("domínio")
+    ) {
+      emailInput.parentElement?.classList.add("field-error");
+    }
+
+    if (msgErro.toLowerCase().includes("informações obrigatórias")) {
+      if (!nameInput.value)
+        nameInput.parentElement?.classList.add("field-error");
+      if (!emailInput.value)
+        emailInput.parentElement?.classList.add("field-error");
+      if (!messageInput.value)
+        messageInput.parentElement?.classList.add("field-error");
+    }
   } finally {
     btnEnviar.disabled = false;
     btnEnviar.innerHTML = 'Enviar Mensagem <i class="ri-send-plane-fill"></i>';
